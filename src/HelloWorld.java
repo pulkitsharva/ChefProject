5d1496870d6fa946e69a66090579707e82e513b6
Pulkit <pulkit.sharva@gmail.com>
2015-03-25 18:16:56 +0530

    change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.Session;
-
-
 public class HelloWorld extends HttpServlet {
 
 	private String message;
